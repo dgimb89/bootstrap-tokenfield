dc6417990e9059143916469fc931e40e2036b097
Peter Retzlaff <peter@playtestcloud.com>
2018-12-10 14:39:34 +0100

    Update dist-files.

--- dist/bootstrap-tokenfield.js
+++ dist/bootstrap-tokenfield.js
@@ -421,14 +421,14 @@
 
           if (e.ctrlKey || e.metaKey) {
             e.preventDefault()
-            return _self.toggle( $token )
+            return _self.toggle( $(this) )
           }
 
-          _self.activate( $token, e.shiftKey, e.shiftKey )
+          _self.activate( $(this), e.shiftKey, e.shiftKey )
         })
         .on('dblclick', function (e) {
           if (_self._disabled || _self._readonly || !_self.options.allowEditing ) return false
-          _self.edit( $token )
+          _self.edit( $(this) )
         })
 
       $closeButton
@@ -881,9 +881,7 @@
   , activateAll: function() {
       var _self = this
 
-      this.$wrapper.find('.token').each( function (i) {
-        _self.activate($(this), i !== 0, false, false)
-      })
+      _self.activate(this.$wrapper.find('.token'), false, false, false);
     }
 
   , deactivate: function($token) {
